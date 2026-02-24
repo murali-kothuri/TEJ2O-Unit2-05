ba6c1218da11fa941dbfaec7a9574707af0b43ed
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-02-24 13:18:51 -0500

    change and commit

--- main.ts
+++ main.ts
@@ -5,4 +5,18 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let currentTemperature: number
+let currentTemperatureIntoKelvin
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  // get light value
+  currentTemperature = input.temperature()
+  currentTemperatureIntoKelvin = (currentTemperature + 273.15)
+  currentTemperatureIntoKelvin = Math.round(currentTemperatureIntoKelvin)
+
+  // output answer
+  basic.clearScreen()
+  basic.showString('The temperature is: ' + currentTemperatureIntoKelvin.toString() + 'K.')
+})
